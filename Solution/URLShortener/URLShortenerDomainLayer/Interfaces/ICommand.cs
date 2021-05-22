e665b19580b13430989a4cf222415e2ef166be48
Alexey Gribov <m0chk0z@gmail.com>
2021-05-22 23:25:00 +0300

    [URLSH-16] Implement IQuery and ICommand interfaces

---
+++ Solution/URLShortener/URLShortenerDomainLayer/Interfaces/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortenerDomainLayer.Interfaces
+{
+    /* 
+     * Command objects represent mutating operations.
+     * Command objects have a return type to return information about operation completion
+     * (such as id of inserted entity).
+    */
+    public interface ICommand<TParam, TOutput> 
+    {
+        public TOutput Execute(TParam param);
+    }
+}
---
+++ Solution/URLShortener/URLShortenerDomainLayer/Interfaces/IQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortenerDomainLayer.Interfaces
+{
+    /* 
+     * Query objects represent non-mutating operations.
+     * The result of query operation can be cached.
+    */
+    public interface IQuery<TParam, TOutput>
+    {
+        public TOutput Execute(TParam param);
+    }
+}
