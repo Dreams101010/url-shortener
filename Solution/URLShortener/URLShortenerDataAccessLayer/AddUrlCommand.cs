9d5c670c8374f6818baa05e8647b39fe489c9cb7
Alexey Gribov <m0chk0z@gmail.com>
2021-05-25 09:11:26 +0300

    [URLSH-24] Implement AddUrlCommand and related MongoDB model

---
+++ Solution/URLShortener/URLShortenerDataAccessLayer/AddUrlCommand.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URLShortenerDataAccessLayer.Models;
+using URLShortenerDomainLayer.Interfaces;
+using URLShortenerDomainLayer.Models;
+
+namespace URLShortenerDataAccessLayer 
+{
+    public class AddUrlCommand : ICommand<AddURLCommandParam, bool>
+    {
+        public MongoClient MongoClient { get; }
+        public AddUrlCommand(MongoClient mongoClient)
+        {
+            MongoClient = mongoClient 
+                ?? throw new ArgumentNullException(nameof(mongoClient));
+        }
+
+        public bool Execute(AddURLCommandParam param)
+        {
+            var model = new UrlMongoModel(param.URLToAdd);
+            MongoClient.GetDatabase("urlDatabase")
+                .GetCollection<UrlMongoModel>("urls")
+                .InsertOne(model);
+            return true;
+        }
+    }
+}
---
+++ Solution/URLShortener/URLShortenerDataAccessLayer/Models/UrlMongoModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using URLShortenerDomainLayer.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace URLShortenerDataAccessLayer.Models
+{
+    public class UrlMongoModel
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string ShortUrl { get; set; }
+        public string LongUrl { get; set; }
+        public DateTime ExpireDateTime { get; set; }
+
+        public UrlMongoModel()
+        {
+
+        }
+
+        public UrlMongoModel(URLDomainModel domainModel)
+        {
+            ShortUrl = domainModel.ShortUrl;
+            LongUrl = domainModel.LongUrl;
+            ExpireDateTime = domainModel.ExpireDateTime;
+        }
+
+        public URLDomainModel GetDomainModel()
+        {
+            return new()
+            {
+                ShortUrl = ShortUrl,
+                LongUrl = LongUrl,
+                ExpireDateTime = ExpireDateTime,
+            };
+        }
+    }
+}
